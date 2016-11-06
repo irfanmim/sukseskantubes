30413109e879211b11d36f551e7d1ce7bd949874
Faiz Haznitrama <Faiz Haznitrama>
2016-11-07 06:53:30 +0700

    ADT Peta + Contoh peta di .txt udah teruji

--- ADT/Peta/peta.c
+++ ADT/Peta/peta.c
@@ -7,12 +7,10 @@ void MakePeta(int LB, int PNJ, PETA *P){
 
 void ReadPeta(FILE *source,PETA *P,str filetxt){
     source = fopen(filetxt,"r");
-    int c = fgetc(source);
     int i = 0;
-    while (c != EOF){
+    while (!feof(source)){
         fgets((*P).peta[i],CharMax,source);
         i++;
-        c = fgetc(source);
     }
     fclose(source);
 }
@@ -24,7 +22,7 @@ void PrintPeta(PETA P){
             if ((j == PanjangPeta(P)) && (i == LebarPeta(P)))
                 printf("%c",Posisi(P,i,j));
             else if (j == PanjangPeta(P))
-                printf("%c\n",Posisi(P,i,j));
+                printf("\n");
             else
                 printf("%c ",Posisi(P,i,j));
         }
@@ -33,10 +31,10 @@ void PrintPeta(PETA P){
 
 void PutarPeta(PETA *P){
     int i,j,k; PETA Pt;
-    MakePeta(LebarPeta(*P),PanjangPeta(*P),&Pt);
-    for(i=LBMin;i<=LebarPeta(*P);i++){
-        for(j=PNJMin;j<=PanjangPeta(*P);j++){
-            k = PanjangPeta(*P)-i+1;
+    MakePeta(PanjangPeta(*P),LebarPeta(*P),&Pt);
+    for(i=LBMin;i<LebarPeta(*P);i++){
+        for(j=PNJMin;j<PanjangPeta(*P);j++){
+            k = LebarPeta(*P)-i-1;
             Posisi(Pt,j,k) = Posisi(*P,i,j);
         }
     }
